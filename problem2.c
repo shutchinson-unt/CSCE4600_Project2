0cb46cfee689fed9397ac604207205a5c4600c12
Scott Hutchinson <scotthutchinson@my.unt.edu>
2014-12-19 08:34:44 -0600

    Integrated custom allocator

--- problem2.c
+++ problem2.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     (void) argc;
     (void) argv;
 
-    Simulator_run(10000000);
+    Simulator_run(10000000, 1);
 
     return 0;
 }
--- simulator.h
+++ simulator.h
@@ -4,6 +4,6 @@
 #include <stddef.h>
 
 
-extern void Simulator_run(size_t initial_memory);
+extern void Simulator_run(size_t initial_memory, int use_custom_allocator);
 
 #endif
