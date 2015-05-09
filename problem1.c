9f6e716332d3334f7f2591f6590ca4b06a5900e2
Scott Hutchinson <scotthutchinson@my.unt.edu>
2015-05-08 19:37:12 -0500

    Final changes for submission

--- problem1.c
+++ problem1.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     (void) argc;
     (void) argv;
 
-    // run simulation
+    // run simulation with at most 5 active processes
     Simulator_run(10000000, 0, 5);
 
     return 0;
--- problem3.c
+++ problem3.c
@@ -6,12 +6,12 @@ int main(int argc, char **argv)
     (void) argc;
     (void) argv;
 
-    // // run simulation with 50% system memory and at most 5 active processes
-    // Simulator_run(10000000 / 2, 0, 5);
-    // // run simulation with 50% system memory and at most 10 active processes
-    // Simulator_run(10000000 / 2, 0, 10);
-    // // run simulation with 50% system memory and unlimited active processes
-    // Simulator_run(10000000 / 2, 0, 0);
+    // run simulation with 50% system memory and at most 5 active processes
+    Simulator_run(10000000 / 2, 0, 5);
+    // run simulation with 50% system memory and at most 10 active processes
+    Simulator_run(10000000 / 2, 0, 10);
+    // run simulation with 50% system memory and unlimited active processes
+    Simulator_run(10000000 / 2, 0, 0);
 
     // run simulation with 10% system memory and at most 5 active processes
     Simulator_run(10000000 / 10, 0, 5);
